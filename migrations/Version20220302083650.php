370dedc52cb0f0ccf0c38c3921c88bf6a63a97b3
Rokas Mikalkėnas <rokas.mikalkenas@nfq.lt>
2022-03-02 10:03:44 +0200

    Versionable column with datetime type issue

--- migrations/Version20220302083650.php
+++ migrations/Version20220302083650.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220224201549 extends AbstractMigration
+final class Version20220302083650 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20220224201549 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE customer (id CHAR(36) NOT NULL COMMENT \'(DC2Type:guid)\', price NUMERIC(17, 4) DEFAULT \'0.00\' NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE customer CHANGE version version TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE customer');
+        $this->addSql('ALTER TABLE customer CHANGE version version DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL');
     }
 }
